e626889637c77893ec32c3f7fff7aab5cd110395
mohsen mokhtari <mr.marsxan@gmail.com>
2022-03-19 02:12:34 +0330

    add app theming

---
+++ lib/core/theme/colors.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+//Light
+const red400 = Color(0xFFFF464F);
+const red300 = Color(0xFFFF575F);
+const red50 = Color(0xFFFFE5E7);
+//Dark
+const red400d = Color(0xFFFF464F);
+const red300d = Color(0xFFFF575F);
+const red50d = Color(0xFF623A42);
+
+
+const orange400 = Color(0xFFFF8A34);
+const orange300 = Color(0xFFFF974A);
+const orange50 = Color(0xFFFFEFE3);
+
+const orange400d = Color(0xFFFF8A34);
+const orange300d = Color(0xFFFF974A);
+const orange50d = Color(0xFF624D3B);
+
+
+const yellow400 = Color(0xFFFFBC25);
+const yellow300 = Color(0xFFFFC542);
+const yellow50 = Color(0xFFFEF3D9);
+
+const yellow400d = Color(0xFFFFBC25);
+const yellow300d = Color(0xFFFFC542);
+const yellow50d = Color(0xFF625B39);
+
+
+const green400 = Color(0xFF25C685);
+const green300 = Color(0xFF3DD598);
+const green50 = Color(0xFFD4F5E9);
+
+const green400d = Color(0xFF25C685);
+const green300d = Color(0xFF3DD598);
+const green50d = Color(0xFF286053);
+
+
+const blue400 = Color(0xFF005DF2);
+const blue300 = Color(0xFF0062FF);
+const blue50 = Color(0xFFE3EEFF);
+
+const blue400d = Color(0xFF005DF2);
+const blue300d = Color(0xFF0062FF);
+const blue50d = Color(0xFF163E72);
+
+
+const violet400 = Color(0xFF6952DC);
+const violet300 = Color(0xFF755FE2);
+const violet50 = Color(0xFFEDEAFD);
+
+const violet400d = Color(0xFF6952DC);
+const violet300d = Color(0xFF755FE2);
+const violet50d = Color(0xFF393D69);
+
+
+const darkGreen400 = Color(0xFF1A3B34);
+const darkGreen300 = Color(0xFF899A96);
+const darkGreen100 = Color(0xFFE4E9F3);
+const darkGreen50 = Color(0xFFEDF1FA);
+
+const darkGreen400d = Color(0xFFFFFFFF);
+const darkGreen300d = Color(0xFF96A7AF);
+const darkGreen100d = Color(0xFF475E69);
+const darkGreen50d = Color(0xFF30444E);
+
+
+
+const greenGradient = LinearGradient(colors: [Color(0xFF40df9f),Color(0xFF3ed598)],begin:Alignment.topLeft,end: Alignment.bottomRight );
+const lightOrangeGradient = LinearGradient(colors: [Color(0xFFfafbfc),Color(0xFFf2f3fb)],begin:Alignment.topLeft,end: Alignment.bottomRight );
+
+const darkOrangeGradient = LinearGradient(colors: [Color(0xFF2a3c44),Color(0xFF23343c)],begin:Alignment.topLeft,end: Alignment.bottomRight );
+const darkGreenGradient = LinearGradient(colors: [Color(0xFF22343c),Color(0xFF1f2e35)],begin:Alignment.topLeft,end: Alignment.bottomRight );
---
+++ lib/core/theme/text_styles.dart
@@ -0,0 +1,136 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:marvie/core/theme/colors.dart';
+
+const primaryFontFamily = "ProductSans";
+
+const whiteCardContent =  TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+    color: Colors.white,
+    fontStyle: FontStyle.normal,
+    fontFamily: primaryFontFamily,
+    decoration: TextDecoration.none);
+
+const whiteButtonTitle =  TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+    color: Colors.white,
+    fontStyle: FontStyle.normal,
+    fontFamily: primaryFontFamily,
+    decoration: TextDecoration.none);
+
+const darkGreenContent =  TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+    color: darkGreen300d,
+    fontStyle: FontStyle.normal,
+    fontFamily: primaryFontFamily,
+    decoration: TextDecoration.none);
+
+const whiteCardTitle =  TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.bold,
+    color: Colors.white,
+    fontStyle: FontStyle.normal,
+    fontFamily: primaryFontFamily,
+    decoration: TextDecoration.none);
+
+const darkGreenCardTitle =  TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.bold,
+    color: darkGreen300d,
+    fontStyle: FontStyle.normal,
+    fontFamily: primaryFontFamily,
+    decoration: TextDecoration.none);
+
+const whiteCardTitle2 =  TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.bold,
+    color: Colors.white,
+    fontStyle: FontStyle.normal,
+    fontFamily: primaryFontFamily,
+    decoration: TextDecoration.none);
+
+const darkGreenCaptionStyle =  TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.w200,
+    color: darkGreen300,
+    fontStyle: FontStyle.normal,
+    fontFamily: primaryFontFamily,
+    decoration: TextDecoration.none);
+
+const darkGreenCaptionStyleD =  TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.w200,
+    color: darkGreen300d,
+    fontStyle: FontStyle.normal,
+    fontFamily: primaryFontFamily,
+    decoration: TextDecoration.none);
+
+
+const darkGreenBodyStyle =  TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+    color: darkGreen300,
+    fontStyle: FontStyle.normal,
+    fontFamily: primaryFontFamily,
+    decoration: TextDecoration.none);
+
+const greenBodyStyle =  TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+    color: green300,
+    fontStyle: FontStyle.normal,
+    fontFamily: primaryFontFamily,
+    decoration: TextDecoration.none);
+
+const whiteBodyStyle =  TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+    fontStyle: FontStyle.normal,
+    color: Colors.white,
+    fontFamily: primaryFontFamily,
+    decoration: TextDecoration.none);
+
+const whiteLargeBodyStyle =  TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.w600,
+    fontStyle: FontStyle.normal,
+    color: Colors.white,
+    fontFamily: primaryFontFamily,
+    decoration: TextDecoration.none);
+
+
+const whiteLargeHeadingStyle =  TextStyle(
+    fontSize: 42,
+    fontWeight: FontWeight.w900,
+    fontStyle: FontStyle.normal,
+    color: Colors.white,
+    fontFamily: primaryFontFamily,
+    decoration: TextDecoration.none);
+
+const whiteMediumHeadingStyle =  TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.w500,
+    fontStyle: FontStyle.normal,
+    color: Colors.white,
+    fontFamily: primaryFontFamily,
+    decoration: TextDecoration.none);
+
+const whiteMediumBoldHeadingStyle =  TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.bold,
+    fontStyle: FontStyle.normal,
+    color: Colors.white,
+    fontFamily: primaryFontFamily,
+    decoration: TextDecoration.none);
+
+const onBoardingTitleStyle =  TextStyle(
+    fontSize: 22,
+    fontWeight: FontWeight.w400,
+    fontStyle: FontStyle.normal,
+    color: Colors.white,
+    fontFamily: primaryFontFamily,
+    decoration: TextDecoration.none);
